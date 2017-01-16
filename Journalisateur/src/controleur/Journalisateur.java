e61aa139d4be7b8f256015ad08960b43cb124f81
invite <invite@localhost>
2017-01-16 16:22:30 +0100

    Troisième dépot

--- Journalisateur/src/controleur/Journalisateur.java
+++ Journalisateur/src/controleur/Journalisateur.java
@@ -20,7 +20,6 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
@@ -32,7 +31,7 @@ public class Journalisateur extends HttpServlet {
 
 	
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("journal");
-	private final static String QUEUE_NAME = "journal";
+	private final static String QUEUE_NAME = "journal-des-authentifications";
 	private String message;
 	private Connection connection = null;
 	private Channel channel = null;
@@ -54,20 +53,28 @@ public class Journalisateur extends HttpServlet {
 	    factory.setHost("rabbitmq");
 	    connection = factory.newConnection();
 	    channel = connection.createChannel();
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);	    
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);	
 	    
-	    Consumer consumer = new DefaultConsumer(channel) {
-	        @Override
-	        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-	            throws IOException {
-	       
-	          message = new String(body, "UTF-8");
-	          System.out.println("MESSAGE =======> " + message);
-	          journaliser();
-	        }
-	      };
-	      System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	      channel.basicConsume(QUEUE_NAME, true, consumer);
+	    boolean autoAck = false;
+	    channel.basicConsume(QUEUE_NAME, autoAck, "journalisateur",
+	         new DefaultConsumer(channel) {
+	             @Override
+	             public void handleDelivery(String consumerTag,
+	                                        Envelope envelope,
+	                                        AMQP.BasicProperties properties,
+	                                        byte[] body)
+	                 throws IOException
+	             {
+	                 String routingKey = envelope.getRoutingKey();
+	                 String contentType = properties.getContentType();
+	                 long deliveryTag = envelope.getDeliveryTag();
+	                 
+	                 message = new String(body, "UTF-8");
+	                 System.out.println(message);
+	   	          	 journaliser();
+	                 channel.basicAck(deliveryTag, false);
+	             }
+	         });	    
 	}
 	
 	private void journaliser() {
@@ -78,7 +85,6 @@ public class Journalisateur extends HttpServlet {
 		Journal journal = new Journal();
 		journal.setEmail(donneesMembres[0]);
 		journal.setUtilisateur(donneesMembres[1]);
-		System.out.println("STATUT " + donneesMembres[2]);
 		journal.setStatut(donneesMembres[2]);
 		journal.setDateacces(new Date());		
 		em.persist(journal);
@@ -89,9 +95,17 @@ public class Journalisateur extends HttpServlet {
 	
 	
 	public Journalisateur() {
+		super();
 	}
 	
 	public void destroy() {
+		try {
+				channel.basicCancel("journalisateur");
+				channel.close();
+				connection.close();
+		} catch (IOException e) {e.printStackTrace();
+		} catch (TimeoutException e) {e.printStackTrace();
+		}		
 		emf.close();
 	}
 
