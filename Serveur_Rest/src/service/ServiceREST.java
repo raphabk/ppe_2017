0e5302caecce53d4381edf32cc1b168104116e8e
invite <invite@localhost>
2017-01-18 16:40:17 +0100

    .

--- Serveur_Rest/src/service/ServiceREST.java
+++ Serveur_Rest/src/service/ServiceREST.java
@@ -12,6 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -22,72 +24,70 @@ import ressources.MessageDTO;
 
 @Path("dto")
 public class ServiceREST {
-	
+
 	private final static String QUEUE_NAME = "journal-des-authentifications";
 	private String messageJournal;
 	private String nomprenom;
 	private String role;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public MessageDTO getDTO(@QueryParam("email") String email, @QueryParam("password") String password) {
-		
+
 		MessageDTO message = new MessageDTO();
-		
-		if(authentifier(email, password)) {
-			message.setBienvenue("Bienvenue " + nomprenom + " ");
+
+		if (authentifier(email, password)) {
+			message.setBienvenue("Bienvenue " + nomprenom);
 			message.setRole(role);
+		} else {
+			String donneesMembres[] = StringUtils.split(messageJournal, "|");
+			message.setBienvenue(donneesMembres[2]);
 		}
-		else
-			message.setBienvenue("Votre compte et/ou mot de passe est incorrect");
-		
-		return message;		
+
+		return message;
 	}
-	
+
 	private boolean authentifier(String email, String password) {
 		boolean statut = false;
 		EntityManager em = null;
 		try {
 			em = FournisseurDePersistance.getInstance().fournir();
-			em.getTransaction().begin();
 			Query requete = em.createNativeQuery("SELECT * FROM UTILISATEUR WHERE EMAIL = ?", Utilisateur.class);
 			requete.setParameter(1, email);
 			Utilisateur utilisateur = (Utilisateur) requete.getSingleResult();
-		
-			if(!utilisateur.getPassword().equals(password)) {
-				messageJournal = email + "|mauvais password|" + "echec|" + new Date();
+			nomprenom = utilisateur.getNom() + " " + utilisateur.getPrenom();
+			role = utilisateur.getRole().getRole();
+			messageJournal = email + "|" + nomprenom + "|";
+			if (!utilisateur.getPassword().equals(password)) {
+				messageJournal += "mauvais password";
+			} else {
+				messageJournal += "succes";
+				statut = true;
 			}
-			else {
-					nomprenom = utilisateur.getNom() + " " + utilisateur.getPrenom()+", ";
-					role=utilisateur.getRole().getRole();
-					messageJournal = email + "|" + nomprenom + "|succes|" + new Date();
-					statut = true;
-			}
-			
-
-	} catch (Exception e) {
-					messageJournal = email + "|utilisateur inconnu|" + "echec|" + new Date();
-		}
-		finally {}
-		
-			em.getTransaction().commit();
-			em.close();
+		} catch (Exception e) {
+			messageJournal = email + "|null|utilisateur inconnu";
+			em.getTransaction().rollback();
+		} finally {
 			try {
-					journaliser();
-			} catch (Exception e) {e.printStackTrace();}			
-		
+				em.close();
+				// emf.close();
+				journaliser();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return statut;
 	}
-	
+
 	private void journaliser() throws IOException, TimeoutException {
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("rabbitmq");
-	    Connection connexion = (Connection) factory.newConnection();
-	    Channel channel = connexion.createChannel();
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	    channel.basicPublish("", QUEUE_NAME, null, messageJournal.getBytes());
-	    System.out.println(" [x] Envoyé '" + messageJournal + "'");
-	    channel.close();
-	    connexion.close();
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("rabbitmq");
+		Connection connexion = (Connection) factory.newConnection();
+		Channel channel = connexion.createChannel();
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		channel.basicPublish("", QUEUE_NAME, null, messageJournal.getBytes());
+		System.out.println(" [x] Envoyé '" + messageJournal + "'");
+		channel.close();
+		connexion.close();
 	}
 }
